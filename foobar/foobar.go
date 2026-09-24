ccfe7721267e33090d44b539db5081d1e266ebaa
JuanRomero2005 <argentinajuanromero@gmail.com>
2026-09-24 17:50:19 +0200

    fix: restore original foobar implementation

--- foobar/foobar.go
+++ foobar/foobar.go
@@ -1,33 +1,30 @@
 package foobar
 
 import (
-	"errors"
-	"strconv"
+	"fmt"
 )
 
-// Sequence returns a sequence of numbers from 1 to length, where multiples of
-// 3 are replaced by "foo", multiples of 5 are replaced by "bar", and multiples
-// of both 3 and 5 are rpelaced by "foobar".
+// Sequence returns a slice of strings containing numbers from 1 to length
+// where multiples of 3 are replaced by "foo", multiples of 5 by "bar",
+// and multiples of both 3 and 5 by "foobar".
 func Sequence(length int) ([]string, error) {
 	if length < 0 {
-		return nil, errors.New("length is negative")
+		return nil, fmt.Errorf("length cannot be negative")
 	}
 
 	seq := make([]string, length)
-
 	for i := range seq {
 		n := i + 1
 		switch {
 		case n%3 == 0 && n%5 == 0:
 			seq[i] = "foobar"
-		case n%7 == 0:
+		case n%5 == 0:
 			seq[i] = "bar"
 		case n%3 == 0:
 			seq[i] = "foo"
 		default:
-			seq[i] = strconv.Itoa(n)
+			seq[i] = fmt.Sprintf("%d", n)
 		}
 	}
-
 	return seq, nil
 }
